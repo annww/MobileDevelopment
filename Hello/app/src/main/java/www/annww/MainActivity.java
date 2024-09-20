c9d5ef2b03143ae05cbb7809cd19425f4534af71
Anh Hong <samh5813p@gmail.com>
2024-09-20 15:39:13 +0700

    no message

--- Hello/app/src/main/java/www/annww/MainActivity.java
+++ Hello/app/src/main/java/www/annww/MainActivity.java
@@ -1,6 +1,9 @@
 package www.annww;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -32,6 +35,25 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void SayHello(View v){
+        // Tim dieu khien
+        EditText soThuNhat = findViewById(R.id.editTextNumber);
+        EditText soThuHai = findViewById(R.id.editTextNumber2);
+        // Lay du lieu
+        String s1 = soThuNhat.getText().toString();
+        String s2= soThuHai.getText().toString();
+        // Chuyen kieu
+        int num1 = Integer.parseInt(s1);
+        double num2 = Double.parseDouble(s2);
+
+        // Tinh toan
+        double tong = num1 + num2;
+
+        // Chuan bi xuat
+        String chuoiXuat = "Tong la: " + String.valueOf(tong);
+        Toast.makeText(this, "You touched me", Toast.LENGTH_LONG).show();
     }
 
 }
