e68cda05639709ff7b60fc03e9a05ed7b2f498dd
Anh Hong <samh5813p@gmail.com>
2024-11-01 15:19:33 +0700

    no message

--- 64132083-MobileAppDev-main/RecyclerView/app/src/main/java/thi/tai64132083/recyclerview/ClickLisenner.java
+++
@@ -1,7 +0,0 @@
-package thi.tai64132083.recyclerview;
-
-public class ClickLisenner {
-    void click(int img, String name, String gia){
-
-    }
-}
--- 64132083-MobileAppDev-main/ThiGiuaKi/app/src/main/java/thi/tai64132083/thigiuaki/adapter/ClickLisenner.java
+++
@@ -1,7 +0,0 @@
-package thi.tai64132083.thigiuaki.adapter;
-
-public class ClickLisenner {
-    void click(int img, String name, String gia){
-
-    }
-}
