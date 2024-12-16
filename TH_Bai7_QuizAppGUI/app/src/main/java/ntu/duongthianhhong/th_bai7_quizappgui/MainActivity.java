16938a83b9d30321196ccc5690ba21e7230eaf5c
Anh Hong <samh5813p@gmail.com>
2024-12-16 19:56:44 +0700

    update adroid ex7

---
+++ TH_Bai7_QuizAppGUI/app/src/main/java/ntu/duongthianhhong/th_bai7_quizappgui/MainActivity.java
@@ -0,0 +1,28 @@
+package ntu.duongthianhhong.th_bai7_quizappgui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Chuyển sang màn hình câu hỏi sau 3 giây
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // 3000ms = 3 giây
+    }
+}
